395d0e92a887f46d2248fd30046f203bb4f38117
Michael Dunne <mpdunne@live.com>
2024-10-12 20:17:59 +0200

    Bug fix in import from base

--- easyloader/__init__.py
+++ easyloader/__init__.py
@@ -1,6 +1,6 @@
-from .loader.array import ArrayDataLoader
-from .loader.df import DFDataLoader
-from .loader.h5 import H5DataLoader
+from .dataset.array import ArrayDataset
+from .dataset.df import DFDataset
+from .dataset.h5 import H5Dataset
 
 from .loader.array import ArrayDataLoader
 from .loader.df import DFDataLoader
